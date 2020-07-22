3fa63509af15e498a8887b4b46f92fe14cba6c9c
Jerzy9 <zbylutkonrad@gmail.com>
2020-07-22 17:00:44 +0200

    before creating get request from REST Api

--- src/main/resources/static/post_request.js
+++ src/main/resources/static/post_request.js
@@ -1,38 +1,55 @@
-var submit_btn = document.getElementById("submit-btn");
+// POST Task
+function sendTaskToRestApi() {
 
-    function sendObject() {
+    const taskData = {
+        name: $("#name-input").val(),
+        text: $("#text-area").val()
+    };
 
-         const taskData = {
-            name: $("#name-input").val(),
-            text: $("#text-area").val()
-        };
-
-        $.ajax({
-            type: "POST",
-            url: "http://localhost:8080/api/v1/task",
-            data: JSON.stringify(taskData),
-            contentType: "application/json; charset=utf-8",
-            dataType: "json",
-            success: function(data) {
-                alert(data);
-            },
-            failure: function(errMsg) {
-                alert(errMsg);
-            }
-        });
-    }
-
-    submit_btn.addEventListener("click", function() {
-        // Prevent the form from submitting via the browser.
-        event.preventDefault();
-
-
-//        console.log(taskData.name);
-//        console.log(text);
-        
-        sendObject();
-
-          
-        
-       
+    $.ajax({
+        type: "POST",
+        url: "http://localhost:8080/api/v1/task",
+        data: JSON.stringify(taskData),
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        success: function(data) {
+            alert(data);
+        },
+        failure: function(errMsg) {
+            alert(errMsg);
+        }
     });
+}
+
+submit_btn.addEventListener("click", function() {
+    // Prevent the form from submitting via the browser.
+    event.preventDefault();
+    
+    sendTaskToRestApi();
+
+});
+
+//GET All Tasks
+function getDateFromRestApi() {
+
+
+$.ajax({
+    type: "GET",
+    url: "http://localhost:8080/api/v1/task",
+    contentType: "application/json; charset=utf-8",
+    success: function(data) {
+        alert(data);
+    },
+    failure: function(errMsg) {
+        alert(errMsg);
+    }
+});
+}
+
+submit_btn.addEventListener("click", function() {
+// Prevent the form from submitting via the browser.
+event.preventDefault();
+
+getDateFromRestApi()
+
+});
