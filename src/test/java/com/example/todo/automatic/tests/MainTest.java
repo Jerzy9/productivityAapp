d7e377a98e4737827fbbfc5f13f647131b81b728
Jerzy9 <zbylutkonrad@gmail.com>
2020-07-28 18:42:24 +0200

    Start web browser with Selenium

---
+++ src/test/java/com/example/todo/automatic/tests/MainTest.java
@@ -0,0 +1,7 @@
+package com.example.todo.automatic.tests;
+
+public class MainTest {
+    public static void main(String[] args) {
+        new StartWebBrowser();
+    }
+}
---
+++ src/test/java/com/example/todo/automatic/tests/StartWebBrowser.java
@@ -0,0 +1,26 @@
+package com.example.todo.automatic.tests;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class StartWebBrowser {
+    private WebDriver driver;
+
+    public StartWebBrowser() {
+        //set property
+        System.setProperty("webdriver.chrome.driver", "D:\\Web Drivers\\chrome\\chromedriver84.exe");
+        driver = new ChromeDriver();
+        startAndSearch();
+    }
+
+    public void startAndSearch() {
+        driver.get("http://localhost:8080");
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
+
+    }
+
+}
