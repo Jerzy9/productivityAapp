bc889c985bae47576a6ed5c2573a9c5b0dab8784
Jerzy9 <zbylutkonrad@gmail.com>
2020-08-10 23:35:31 +0200

    delete task tests, comments and bug fix(catch correct exception)

--- src/test/java/com/example/todo/automatic/tests/tests/TaskHomePageTest.java
+++ src/test/java/com/example/todo/automatic/tests/tests/TaskHomePageTest.java
@@ -20,6 +20,7 @@ public class TaskHomePageTest {
     }
 
     private void openFormIfClosed() {
+        //open form if closed or do nothing if it's open
         if(!homePage.isFormShown()) {
             homePage.clickOnAddTask();
         }
@@ -38,6 +39,7 @@ public class TaskHomePageTest {
 
     @Test
     public void givenTaskFrom_whenInsertNameTextAndSubmit_thenTaskAdded() {
+        //generate random name and text
         String name = config.randomStringGenerator(10);
         String text = config.randomStringGenerator(32);
 
@@ -51,12 +53,15 @@ public class TaskHomePageTest {
             ex.fillInStackTrace();
         }
 
+        //is there taskName? yes
         assertEquals(homePage.getLatestTaskName(), name);
+        //is there taskText? yes
         assertEquals(homePage.getLatestTaskText(), text);
     }
 
     @Test
     public void givenTaskFrom_whenInsertOnlyNameAndSubmit_thenTaskAdded() {
+        //generate random name
         String name = config.randomStringGenerator(10);
 
         openFormIfClosed();
@@ -64,14 +69,15 @@ public class TaskHomePageTest {
         homePage.clickOnAddTask();
 
         try {
-            Thread.sleep(3000);
+            Thread.sleep(1000);
         } catch (InterruptedException ex) {
             ex.fillInStackTrace();
         }
-
+        //is there name? yes
         assertEquals(homePage.getLatestTaskName(), name);
+        //is there text? no
         try {
-            assertNull(homePage.getLatestTaskText());
+            assertNotEquals(homePage.getLatestTaskText(), null);
         } catch (NoSuchElementException ex) {
             assertThrows(NoSuchElementException.class, () -> {
                 homePage.getLatestTaskText();
@@ -81,11 +87,12 @@ public class TaskHomePageTest {
 
     @Test
     public void givenTask_whenPressedBinTask_thenDeleteTask() {
+        //generate random name and text
         String name = config.randomStringGenerator(10);
         String text = config.randomStringGenerator(52);
 
         openFormIfClosed();
-
+        //close taskForm
         homePage.addTask(name, text);
 
         try {
@@ -94,6 +101,7 @@ public class TaskHomePageTest {
             ex.fillInStackTrace();
         }
 
+        //is there new added task? yes
         assertEquals(homePage.getLatestTaskName(), name);
         assertEquals(homePage.getLatestTaskText(), text);
 
@@ -103,7 +111,7 @@ public class TaskHomePageTest {
         } catch (InterruptedException ex) {
             ex.fillInStackTrace();
         }
-
+        // is there name? No
         try {
             assertNotEquals(homePage.getLatestTaskName(), name);
         } catch (NoSuchElementException ex) {
@@ -111,7 +119,7 @@ public class TaskHomePageTest {
                 homePage.getLatestTaskName();
             });
         }
-
+        //is there text? No
         try {
             assertNotEquals(homePage.getLatestTaskText(), text);
         } catch (NoSuchElementException ex) {
