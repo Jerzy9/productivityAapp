ce9ae3b684d97b42adf96d86e6dcd9081bf8cc08
Jerzy9 <zbylutkonrad@gmail.com>
2020-08-20 18:54:15 +0200

    postgres works

--- src/test/java/com/example/todo/unitTests/TaskDaoAccessServiceTest.java
+++ src/test/java/com/example/todo/unitTests/TaskDaoAccessServiceTest.java
@@ -1,13 +1,10 @@
 package com.example.todo.unitTests;
 
-import com.example.todo.dao.TaskDao;
 import com.example.todo.dao.TaskDaoAccessService;
 import com.example.todo.model.Task;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 
 import static org.junit.Assert.*;
 
